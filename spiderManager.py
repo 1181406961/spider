f59bb4267220a565eb30cccce7bd32c37e342692
chenjunwei <chenjunwei@zhibenyuan.cc>
2022-08-10 12:15:57 +0800

    优化代码格式

--- spiderManager.py
+++ spiderManager.py
@@ -1,30 +1,32 @@
 from htmldownload import HtmlDownloader
 from htmlparse import HtmlParser
-from urlmanager import UrlManager
 from savedatabase import DataOutput
+from urlmanager import UrlManager
 
-class SpiderMan():
+
+class SpiderMan(object):
     def __init__(self):
         self.manager = UrlManager()
         self.downloader = HtmlDownloader()
         self.parser = HtmlParser()
         self.output = DataOutput()
 
-    def crawl(self,root_url):
+    def crawl(self, root_url):
         self.manager.add_new_url(root_url)
-        while (self.manager.has_new_url() and self.manager.old_url_size()<100):
+        while self.manager.has_new_url() and self.manager.old_url_size() < 100:
             try:
-                new_url =self.manager.get_new_url()
+                new_url = self.manager.get_new_url()
                 html = self.downloader.download(new_url)
-                new_urls,data = self.parser.parser(new_url,html)
+                new_urls, data = self.parser.parser(new_url, html)
                 self.manager.add_new_urls(new_urls)
                 self.output.store_data(data)
-                print("已经抓取%s个链接"%self.manager.old_url_size())
-            except Exception as e:
-                print('crawl failed')
+                print("已经抓取%s个链接" % self.manager.old_url_size())
+            except Exception:
+                import traceback
+                print(f'crawl failed:\n{traceback.format_exc()}')
         self.output.output_html()
+
 
 if __name__ == '__main__':
     spider_man = SpiderMan()
-    spider_man.crawl('https://baike.baidu.com/item/%E7%BD%91%E7%BB%9C%E7%88%AC%E8%99%AB/5162711?fr=aladdin')
-
+    spider_man.crawl('https://baike.baidu.com/item/网络爬虫/5162711?fr=aladdin')
